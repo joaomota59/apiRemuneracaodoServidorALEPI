d1d4b9651f8e665bb18f515011dece71a5dcd76f
unknown <joaomota59@ufpi.edu.br>
2023-03-20 18:21:17 -0300

    ajustes no response da rota da folha de pagamento

--- apiALEPI.py
+++ apiALEPI.py
@@ -1,13 +1,21 @@
 from alepiLibrary import AlepiLibrary
 from fastapi import FastAPI, Query, Response, status
+from pydantic import BaseModel
 
 app = FastAPI(title="API ALEPI", description="API para consulta da Remuneração do Servidor", version="0.1.0")
 
 api = AlepiLibrary()
 
-@app.get("/folha", status_code=status.HTTP_200_OK)
+class Item(BaseModel):
+    id: str
+    value: str
+
+class Message(BaseModel):
+    message: str
+
+@app.get("/folha", status_code=status.HTTP_200_OK, responses={404: {"model": Message}})
 async def read_item(
-    response: Response, 
+    response: Response,
     ano: int = Query(2023, description="Ano da Folha de Pagamento"), 
     mes: int = Query(1, description="Mês da Folha de Pagamento 1=Janeiro, 2=Fevereiro, 3=Março ..."), 
     page: int = Query(1, description="Paginação da Folha de pagamento 1=Primeira página, 2=Segunda página ...")
